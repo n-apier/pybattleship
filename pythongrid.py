81fa565a2bc9db7e119ab87a51b2a6cffcef0024
n_apier <69126935+n-apier@users.noreply.github.com>
2021-05-18 12:47:27 -0700

    Initializing Repository

---
+++ pythongrid.py
@@ -0,0 +1,17 @@
+rows, cols = (5,5)
+arr = [[0]*cols]*rows
+runOnce = False
+
+#for row in arr:
+   # while runOnce:
+      #  print(arr[0][0])
+       # runOnce = True
+    # arr[0][0] += 1
+   # print(row)
+
+# for row in arr:
+    # arr[0][0] += 1
+    # print(row)
+  
+    ## prints 0,1,1,2,2 etc
+
