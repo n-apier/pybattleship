2a672c31462a993cef70233e018bde258c652afe
n_apier <69126935+n-apier@users.noreply.github.com>
2021-08-09 14:57:25 -0700

    Refactoring
    Total rewrite for cleanliness & better practices, added a "Try again" functionality that doesn't add to the user turn

--- pythongrid.py
+++ pythongrid.py
@@ -1,55 +1,56 @@
 from random import randint
 
+battleship_Board = []
 
-board = []
+def buildBoard():
+    for i in range(0,5):
+        battleship_Board.append(["0"]*5)
 
-for i in range(0,5):
-    board.append(["0"]*5)
-
-def print_board(board):
-    for row in board:
+def print_board(battleship_Board):
+    for row in battleship_Board:
         print("".join(row))
 
-print_board(board)
+def setShip(battleship_Board):
+    ship_row = randint(0, len(battleship_Board[0]) - 1)
+    ship_col = randint(0, len(battleship_Board[0]) - 1)
+    return ship_row, ship_col
 
-def random_row(board):
-    return randint(0, len(board[0]) - 1)
-
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
-
-ship_row = random_row(board)
-ship_col = random_col(board)
-print(ship_row)
-print(ship_col)
-sank = 0
-
-for turn in range(4):
-    print("Turn", turn + 1)
-    print("Ships Sank: " + str(sank))
-    guess_row = int(input("Guess Row: "))
-    guess_col = int(input("Guess Col: "))
-
-    
-
-    if guess_row == ship_row and guess_col == ship_col:
-        print("Congratulations you sank my battleship!")
-        sank += 1
-        print("Ships Sank: " + str(sank))
-        print("Game over! You won.")
-        break
+def getRow_Col():
+    guess_row, guess_col = int(input("Guess Row: ")), int(input("Guess Col: "))
+    if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+        print("Whoa! That's not even the ocean my guy, try again.")
+        return getRow_Col()
+    elif(battleship_Board[guess_row][guess_col] == "X"):
+        print("You already guessed this one, try again.")
+        return getRow_Col()
     else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print("Oops that's not even in the ocean.")
-        elif(board[guess_row][guess_col] == "X"):
-            print("You guessed that one already")
+        row = guess_row
+        col = guess_col
+        return row, col
+
+
+def startGame():
+    buildBoard()
+    print_board(battleship_Board)
+    ship_row, ship_col = setShip(battleship_Board)
+    sank = 0
+    turn = 0
+    while turn < 4:
+        print("Turn: " + str(turn))
+        print("Ships Sank: " + str(sank))
+        guess_row, guess_col = getRow_Col()
+        if guess_row == ship_row and guess_col == ship_col:
+            print("Congratulations you sank my battleship!")
+            sank += 1
+            print("Ships Sank: " + str(sank))
+            print("Game over! You won.")
+            break
         else:
+            turn += 1
             print("You missed my battleship!")
-            board[guess_row][guess_col] = "X"
-            print_board(board)
-        if turn == 3 and sank > 0:
-            print("Game over!")
-            print("You won!")
-        elif(turn == 3 and sank < 1):
-            print("Game over!")
-            print("You lost!")
+            battleship_Board[guess_row][guess_col] = "X"
+            print_board(battleship_Board)
+            if turn == 4 and sank < 1:
+                    print("Game over!")
+                    print("You lost!")               
+startGame()
